59a948f22cf37905efccf6fbe8fa0e957559546d
Sakarin Radchapunya <sakarin.my@gmail.com>
2012-05-17 09:59:57 +0700

    init spree ecommerce done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shop::Application.config.secret_token = 'e4b70a62f6fe5e331c3563ad085b703616bd481529509a564984c89e12b930da159c4f281ee8af0d7628557aa002893a09dc84e7b4e6e1d42c80bd9d85c31060'

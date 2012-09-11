33c51bed1307858b4b2ef6460e6a93fab087ba0d
sakarin <sakarin.my@gmail.com>
2012-09-11 14:46:35 +0700

    done

--- lib/spree-multi-currency/app/controllers/spree/base_controller_decorator.rb
+++ lib/spree-multi-currency/app/controllers/spree/base_controller_decorator.rb
@@ -8,6 +8,8 @@ module Spree
     def set_currency
       if session[:currency_id].present? && (@currency = Currency.find_by_char_code(session[:currency_id]))
         Currency.current!(@currency)
+      else
+        session[:currency_id] = "GBP"
       end
 
     end

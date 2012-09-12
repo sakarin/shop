12370ab201075b3a98b56b46f61f4ad9dae7fe5d
sakarin <sakarin.my@gmail.com>
2012-09-12 15:48:43 +0700

    done

--- lib/spree_flexi_variants/app/overrides/add_customizations_to_cart_form.rb
+++ lib/spree_flexi_variants/app/overrides/add_customizations_to_cart_form.rb
@@ -1,4 +1,4 @@
     Deface::Override.new(:virtual_path => "spree/products/_cart_form",
                          :name => "converted_product_price_290656527",
-                         :insert_after => "[data-hook='inside-product-cart-form'], #inside-product-cart-form[data-hook]",
+                         :insert_after => "[data-hook='inside_product_cart_form'], #inside_product_cart_form[data-hook]",
                          :partial => "spree/products/customizations")

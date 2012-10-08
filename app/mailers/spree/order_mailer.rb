f1e50e4e63220e569a098a682d7e900d8f3d997e
sakarin <sakarin.my@gmail.com>
2012-10-08 13:37:20 +0700

    done

--- app/mailers/spree/order_mailer.rb
+++ app/mailers/spree/order_mailer.rb
@@ -1,27 +1,9 @@
 class Spree::OrderMailer < ActionMailer::Base
   helper "spree/base"
-  before_filter :set_mail_method
+
 
   def confirm_email(order, resend=false)
-    @order = order
-    subject = (resend ? "[RESEND] " : "")
-    subject += "#Order Confirmation ##{order.number}"
-    mail_params = {:to => order.email, :subject => subject}
-    mail_params[:from] = order.store.email if order.store.email.present?
-    mail(mail_params)
-  end
 
-  def cancel_email(order, resend=false)
-    @order = order
-    subject = (resend ? "[RESEND] " : "")
-    subject += "#Cancellation of Order ##{order.number}"
-    mail_params = {:to => order.email, :subject => subject}
-    mail_params[:from] = order.store.email if order.store.email.present?
-    mail(mail_params)
-  end
-
-  private
-  def set_mail_method
     ActionMailer::Base.smtp_settings = {
         :address   => "smtp.gmail.com",
         :port                 => 587,
@@ -31,5 +13,33 @@ class Spree::OrderMailer < ActionMailer::Base
         :authentication       => "plain",
         :enable_starttls_auto => true
     }
+
+    @order = order
+    subject = (resend ? "[RESEND] " : "")
+    subject += "#Order Confirmation ##{order.number}"
+    mail_params = {:to => order.email, :subject => subject}
+    mail_params[:from] = order.store.email if order.store.email.present?
+    mail(mail_params)
   end
+
+  def cancel_email(order, resend=false)
+    ActionMailer::Base.smtp_settings = {
+        :address   => "smtp.gmail.com",
+        :port                 => 587,
+        :domain               => "gmail.com",
+        :user_name => order.store.mail_username,
+        :password => order.store.mail_password,
+        :authentication       => "plain",
+        :enable_starttls_auto => true
+    }
+
+    @order = order
+    subject = (resend ? "[RESEND] " : "")
+    subject += "#Cancellation of Order ##{order.number}"
+    mail_params = {:to => order.email, :subject => subject}
+    mail_params[:from] = order.store.email if order.store.email.present?
+    mail(mail_params)
+  end
+
+
 end
--- app/mailers/spree/shipment_mailer.rb
+++ app/mailers/spree/shipment_mailer.rb
@@ -1,19 +1,8 @@
 class Spree::ShipmentMailer < ActionMailer::Base
   helper "spree/base"
-  before_filter :set_mail_method
+
 
   def shipped_email(shipment, resend=false)
-    @shipment = shipment
-    subject = (resend ? "[RESEND] " : "")
-    subject += "#Shipment Notification ##{shipment.order.number}"
-    mail_params = {:to => shipment.order.email, :subject => subject}
-    mail_params[:from] = shipment.order.store.email if shipment.order.store.email.present?
-    mail(mail_params)
-  end
-
-  private
-  def set_mail_method
-
     ActionMailer::Base.smtp_settings = {
         :address   => "smtp.gmail.com",
         :port                 => 587,
@@ -23,6 +12,14 @@ class Spree::ShipmentMailer < ActionMailer::Base
         :authentication       => "plain",
         :enable_starttls_auto => true
     }
+    @shipment = shipment
+    subject = (resend ? "[RESEND] " : "")
+    subject += "#Shipment Notification ##{shipment.order.number}"
+    mail_params = {:to => shipment.order.email, :subject => subject}
+    mail_params[:from] = shipment.order.store.email if shipment.order.store.email.present?
+    mail(mail_params)
   end
+
+
 
 end
